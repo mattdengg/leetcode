a875e0cf5aa3965db1d07d2e68939fdec10f05ce
Matthew Deng <m29deng@uwaterloo.ca>
2025-10-30 18:30:16 -0400

    Initial Commit

---
+++ Data Structures and Algorithms/Data_Structures_and_AlgorithmsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Data_Structures_and_AlgorithmsApp.swift
+//  Data Structures and Algorithms
+//
+//  Created by Matthew Deng on 2025-10-30.
+//
+
+import SwiftUI
+
+@main
+struct Data_Structures_and_AlgorithmsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

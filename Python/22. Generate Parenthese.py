b252512df42b0f2fe2705e1f0f1c15765929e2d9
Matthew Deng <m29deng@uwaterloo.ca>
2026-06-29 00:14:48 -0400

    22. Generate Parenthesis

---
+++ Python/22. Generate Parenthese.py
@@ -0,0 +1,47 @@
+#
+# @lc app=leetcode id=22 lang=python3
+#
+# [22] Generate Parenthesis
+#
+
+# @lc code=start
+# Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+#
+#
+#
+# Example 1:
+#
+# Input: n = 3
+# Output: ["((()))","(()())","(())()","()(())","()()()"]
+# Example 2:
+#
+# Input: n = 1
+# Output: ["()"]
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+
+        stack = []
+        result = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                result.append("".join(stack))
+                return
+
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+
+            if closedN < openN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+
+        backtrack(0, 0)
+        return result
+
+
+
+# @lc code=end
